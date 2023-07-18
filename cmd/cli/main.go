20dfdd963ac11ac87830b6d970f4260518a6f189
James Scott Jr <skoona@gmail.com>
2023-07-18 00:18:40 -0400

    Moved APC to a service.go

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,169 +2,59 @@ package main
 
 import (
 	"context"
-	"encoding/binary"
+	"fmt"
+	"github.com/skoona/ggapcmon/internal/services"
 	"log"
-	"net"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 )
 
 const (
-	defaultPort   = 3551
-	HostVserv     = "10.100.1.3:3551"
-	HostPve       = "10.100.1.4:3551"
-	commandStatus = "status"
-	commandEvents = "events"
+	HostVserv = "10.100.1.3:3551"
+	HostPve   = "10.100.1.4:3551"
 )
 
-func SendCommand(conn net.Conn, command string) error {
-	startTime := time.Now()
-	var msgLen = uint16(len(command))
-	b := make([]byte, 2)
-
-	binary.BigEndian.PutUint16(b, msgLen)
-	_, err := conn.Write(b)
-	if err != nil {
-		log.Println("SendCommand(a) Duration mcs: ", time.Until(startTime).Microseconds())
-		log.Println("Write len Error: ", err.Error())
-		return err
-	}
-
-	_, err = conn.Write([]byte(command))
-	if err != nil {
-		log.Println("SendCommand(b) Duration mcs: ", time.Until(startTime).Microseconds())
-		log.Println("Write command Error: ", err.Error())
-		return err
-	}
-
-	log.Println("SendCommand(c) Duration mcs: ", time.Until(startTime).Microseconds())
-	return nil
-}
-
-func ReceiveMessage(conn net.Conn) (string, error) {
-	var msgLen uint16
-	message := []byte{}
-	b := make([]byte, 2)
-	startTime := time.Now()
-
-	read, err := conn.Read(b)
-
-	msgLen = binary.BigEndian.Uint16(b)
-	if (read == 2 && msgLen == 0) || (read == 0 && msgLen == 0) {
-		log.Println("ReceiveMessage(a) Duration ms: ", time.Until(startTime).Milliseconds(), ", read: ", read, ", msgLen: ", msgLen)
-		return string(message), nil
-
-	} else if err != nil {
-		log.Println("ReceiveMessage(b) Duration mcs: ", time.Until(startTime).Microseconds(), ", read: ", read, ", msgLen: ", msgLen)
-		log.Println("Read len Error: ", err.Error())
-		return "", err
-	}
-
-	line := make([]byte, msgLen)
-
-	read, err = conn.Read(line)
-	if err != nil {
-		log.Println("ReceiveMessage(c) Duration mcs: ", time.Until(startTime).Microseconds())
-		log.Println("Read message Error: ", err.Error())
-		return string(message), err
-	}
-	if read > 2 {
-		message = append(message, line...)
-	}
-	if msgLen != uint16(read) && read != 0 {
-		secondLine := make([]byte, msgLen)
-		for x := read; x <= int(msgLen); {
-			read, err = conn.Read(secondLine)
-			if read > 2 {
-				message = append(message, secondLine[0:read]...)
-			} else {
-				break
-			}
-			if err != nil {
-				log.Println("ReceiveMessage(x) Duration mcs: ", time.Until(startTime).Microseconds(), ", read: ", read, ", msgLen: ", msgLen)
-				log.Println("Read x Error: ", err.Error())
-				break
-			}
-			x = x + read
-		}
-	}
-
-	log.Println("ReceiveMessage(d) Duration mcs: ", time.Until(startTime).Microseconds(), ", read: ", read, ", msgLen: ", msgLen)
-	return string(message), err
-}
-
-func ApcRequest(conn net.Conn, command string) ([]string, error) {
-	var data []string
-
-	err := SendCommand(conn, command)
-
-transact:
-	for err == nil {
-		msg, err := ReceiveMessage(conn)
-		if err != nil {
-			break transact
-		}
-		if len(msg) != 0 {
-			data = append(data, msg)
-		} else {
-			break transact
-		}
-	}
-
-	return data, err
-}
-
 func main() {
-	var d net.Dialer
-
 	systemSignalChannel := make(chan os.Signal, 1)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	defer cancel()
+	msgs := make(chan string, 100)
+	var err error
+
+	ctx, cancelApc := context.WithCancel(context.Background())
+	defer cancelApc()
 
 	logger := log.New(os.Stdout, "[DEBUG] ", log.Lmicroseconds|log.Lshortfile)
 
 	go func(stopFlag chan os.Signal) {
 		signal.Notify(stopFlag, syscall.SIGINT, syscall.SIGTERM)
 		sig := <-stopFlag // wait on ctrl-c
-		logger.Println("Signal Received: ", sig.String())
-		cancel()
+		cancelApc()
+		time.Sleep(5 * time.Second)
+		err = fmt.Errorf("Shutdown Signal Received: %v", sig.String())
 	}(systemSignalChannel)
 
-	startTime := time.Now()
-	conn, err := d.DialContext(ctx, "tcp", HostPve)
-	if err != nil {
-		log.Panic("Dial Error: ", err.Error())
-	}
-	defer func(conn net.Conn) {
-		err := conn.Close()
-		if err != nil {
-			log.Println("Close() Error: ", err.Error())
-		}
-	}(conn)
-	log.Println("Dial Duration ms: ", time.Until(startTime).Microseconds())
+	apc := services.NewServer(ctx, "VServ", HostVserv, 5)
+	defer apc.PeriodicUpdateStop()
+	defer apc.End()
+	apc.Begin()
+	err = apc.PeriodicUpdateStart(msgs)
 
-	ApcInfo := map[string][]string{}
+basic:
+	for {
+		select {
+		case <-ctx.Done():
+			logger.Println("main::Done() fired:", ctx.Err().Error())
+			err = ctx.Err()
+			apc.PeriodicUpdateStop()
+			apc.End()
+			break basic
 
-	result, err := ApcRequest(conn, commandStatus)
-	if err != nil {
-
-	} else {
-		ApcInfo[commandStatus] = result
-	}
-
-	result, err = ApcRequest(conn, commandEvents)
-	if err != nil {
-
-	} else {
-		ApcInfo[commandEvents] = result
-	}
-
-	for key, value := range ApcInfo {
-		for _, msg := range value {
-			log.Print(key, " ==> ", msg)
+		case msg := <-msgs:
+			parts := strings.SplitN(msg, ": ", 2)
+			logger.Print("[", parts[0], "] ==> ", parts[1])
 		}
 	}
-
+	logger.Println("Shutdown:", err.Error())
 }
---
+++ internal/services/server.go
@@ -0,0 +1,229 @@
+package services
+
+import (
+	"context"
+	"encoding/binary"
+	"log"
+	"net"
+	"time"
+)
+
+const (
+	commandStatus = "status"
+	commandEvents = "events"
+)
+
+type APCServer interface {
+	Name() string
+	SetName(newValue string)
+	IpAddress() string
+	SetIpAddress(newValue string)
+	Begin() error
+	AddEvent(newValue string)
+	AddStatus(newValue string)
+	Events() []string
+	Status() []string
+	PeriodicUpdateStart(r chan string) error
+	PeriodicUpdateStop() error
+	Request(command string, r chan string) error
+	SendCommand(command string) error
+	ReceiveMessage() (string, error)
+	End() error
+}
+
+type apcServer struct {
+	ctx           context.Context
+	ipAddress     string
+	name          string
+	periodTicker  *time.Ticker
+	samplePeriod  time.Duration
+	activeSession net.Conn
+	events        []string
+	status        []string
+	tickerChan    chan bool
+	dialer        net.Dialer
+}
+
+var _ APCServer = (*apcServer)(nil)
+
+func NewServer(ctx context.Context, name, ip string, secondsBetweenSamples time.Duration) APCServer {
+	return &apcServer{
+		ctx:          ctx,
+		ipAddress:    ip,
+		name:         name,
+		samplePeriod: secondsBetweenSamples,
+		status:       []string{},
+		events:       []string{},
+		tickerChan:   make(chan bool),
+		dialer:       net.Dialer{},
+	}
+}
+func (a *apcServer) Begin() error {
+
+	if a.activeSession != nil {
+		return nil
+	}
+	if a.ctx.Err() != nil {
+		return a.ctx.Err()
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	conn, err := a.dialer.DialContext(ctx, "tcp", a.ipAddress)
+	if err != nil {
+		log.Println("Begin() dial Error: ", err.Error(), ", host: ", a.name, ", context: ", ctx.Err())
+	} else {
+		a.activeSession = conn
+	}
+	return err
+}
+func (a *apcServer) End() error {
+	if a.activeSession == nil {
+		return nil
+	}
+	err := a.activeSession.Close()
+	if err != nil {
+		log.Println("Close() Error: ", err.Error())
+	}
+	a.activeSession = nil
+
+	return err
+}
+
+func (a *apcServer) Name() string {
+	return a.name
+}
+func (a *apcServer) SetName(newValue string) {
+	a.name = newValue
+}
+func (a *apcServer) IpAddress() string {
+	return a.ipAddress
+}
+func (a *apcServer) SetIpAddress(newValue string) {
+	a.ipAddress = newValue
+}
+func (a *apcServer) AddEvent(newValue string) {
+	a.events = append(a.events, newValue)
+}
+func (a *apcServer) AddStatus(newValue string) {
+	a.status = append(a.status, newValue)
+}
+func (a *apcServer) Events() []string {
+	return append([]string{}, a.events...)
+}
+func (a *apcServer) Status() []string {
+	return append([]string{}, a.status...)
+}
+func (a *apcServer) PeriodicUpdateStart(r chan string) error {
+	a.periodTicker = time.NewTicker(a.samplePeriod * time.Second)
+
+	go func(rcvr chan string) {
+	back:
+		for {
+			select {
+			case <-a.ctx.Done():
+				close(rcvr)
+				log.Println("PeriodicUpdateStart() ending: ", a.ctx.Err().Error())
+				break back
+
+			case <-a.tickerChan:
+				break back
+
+			case <-a.periodTicker.C:
+				_ = a.Request(commandStatus, rcvr)
+				_ = a.Request(commandEvents, rcvr)
+			}
+		}
+		log.Println("PeriodicUpdateStart() ended ")
+	}(r)
+
+	return nil
+}
+func (a *apcServer) PeriodicUpdateStop() error {
+	a.periodTicker.Stop()
+	a.tickerChan <- true
+	return nil
+}
+
+func (a *apcServer) SendCommand(command string) error {
+	var msgLen = uint16(len(command))
+	b := make([]byte, 2)
+
+	binary.BigEndian.PutUint16(b, msgLen)
+	_, err := a.activeSession.Write(b)
+	if err != nil {
+		log.Println("SendCommand() write len error: ", err.Error())
+		return err
+	}
+
+	_, err = a.activeSession.Write([]byte(command))
+	if err != nil {
+		log.Println("SendCommand() write command error: ", err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func (a *apcServer) ReceiveMessage() (string, error) {
+	var msgLen uint16
+	message := []byte{}
+	b := make([]byte, 2)
+
+	read, err := a.activeSession.Read(b)
+	if err != nil {
+		log.Println("ReceiveMessage() read len error: ", err.Error())
+		return "", err
+	}
+
+	msgLen = binary.BigEndian.Uint16(b)
+	if (read == 2 && msgLen == 0) || (read == 0 && msgLen == 0) || (msgLen > 1024) {
+		return "", nil
+	}
+
+	line := make([]byte, msgLen)
+
+	read, err = a.activeSession.Read(line)
+	if err != nil {
+		log.Println("ReceiveMessage() read message error: ", err.Error())
+		return string(message), err
+	}
+	if read > 2 {
+		message = append(message, line[0:read]...)
+	}
+
+	return string(message), err
+}
+
+func (a *apcServer) Request(command string, r chan string) error {
+	err := a.SendCommand(command)
+	if err != nil {
+		log.Println("Request::SendCommand() send command error: ", err.Error())
+		return err
+	}
+	if command == commandEvents {
+		a.events = a.events[:0]
+	} else {
+		a.status = a.status[:0]
+	}
+
+transact:
+	for err == nil {
+		msg, err := a.ReceiveMessage()
+		if err != nil {
+			break transact
+		}
+		if len(msg) == 0 {
+			break transact
+		}
+		if command == commandEvents {
+			a.AddEvent(msg)
+		} else {
+			a.AddStatus(msg)
+		}
+		r <- command + ": " + msg
+	}
+
+	return err
+}
